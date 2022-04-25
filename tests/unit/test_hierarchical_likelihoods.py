78b58ca584847ce9b9ac333b20acb14204820de9
Leser <pleser@ndc.nasa.gov>
2022-04-25 18:34:02 -0400

    added mvnormal model to use with approx hierarch likelihood

--- tests/unit/test_hierarchical_likelihoods.py
+++ tests/unit/test_hierarchical_likelihoods.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from smcpy.hierarch_log_likelihoods import ApproxHierarch
+from smcpy.hierarch_log_likelihoods import ApproxHierarch, MVNHierarchModel
 from smcpy.log_likelihoods import BaseLogLike
 
 
@@ -15,14 +15,14 @@ def test_import_approx_hierarch():
 
 
 def test_approx_hierarch_call(mocker):
-    n_particles = 15
+    n_samples = 15
     nre1 = 3
     nre2 = 3
     nre3 = 5
 
-    conditionals = [np.tile(np.arange(1, nre1 + 1), (n_particles, 1)),
-                    np.tile(np.arange(1, nre2 + 1), (n_particles, 1)),
-                    np.tile(np.arange(1, nre3 + 1), (n_particles, 1))]
+    conditionals = [np.tile(np.arange(1, nre1 + 1), (n_samples, 1)),
+                    np.tile(np.arange(1, nre2 + 1), (n_samples, 1)),
+                    np.tile(np.arange(1, nre3 + 1), (n_samples, 1))]
     model = mocker.Mock(side_effect=[np.log(conditionals[0]),
                                      np.log(conditionals[1]),
                                      np.log(conditionals[2])])
@@ -35,11 +35,11 @@ def test_approx_hierarch_call(mocker):
     model_class = mocker.Mock(return_value=model)
     marginal_log_likes = np.log([1, 2, 3])
     args = (marginal_log_likes, data_log_priors)
-    inputs = np.ones((n_particles, 5))
+    inputs = np.ones((n_samples, 5))
 
     expected_like = np.product([1 * 6 / 5, 2 * 6 / 7, 3 * 15 / 9])
     expected_log_like = np.log(expected_like)
-    expected_log_likes = np.tile(expected_log_like, (n_particles, 1))
+    expected_log_likes = np.tile(expected_log_like, (n_samples, 1))
 
     like = ApproxHierarch(model_class, data, args)
     log_likes = like(inputs)
@@ -50,3 +50,40 @@ def test_approx_hierarch_call(mocker):
         np.testing.assert_array_equal(call, d)
 
     np.testing.assert_array_equal(log_likes, expected_log_likes)
+
+
+@pytest.mark.parametrize('n_inputs,n_hyper', [(2, 3), (4, 10)])
+def test_hierarch_mvnormal_model_init(n_inputs, n_hyper):
+    n_samples = 10
+    n_total = n_inputs + n_hyper
+    inputs = np.tile(np.arange(n_total), (n_samples, 1))
+    model = MVNHierarchModel(inputs)
+
+    np.testing.assert_array_equal(model._inputs, inputs[:, :n_inputs])
+    np.testing.assert_array_equal(model._hyperparams, inputs[:, n_inputs:])
+
+
+@pytest.mark.parametrize('n_inputs,n_hyper', [(1, 3), (7, 5), (15, 294)])
+def test_hierarch_mvnormal_model_bad_dim(n_inputs, n_hyper):
+    n_total = n_inputs + n_hyper
+    inputs = np.tile(np.arange(n_total), (10, 1))
+    with pytest.raises(IndexError):
+        model = MVNHierarchModel(inputs)
+
+def test_hiearch_mvnormal_model_call():
+    num_posterior_samples = 10
+    num_samples = 3
+
+    proposed_params = np.array([[0, 2]] * num_samples)
+    proposed_hyperparams = np.array([[4, -1, 2]] * num_samples)
+    proposal = np.hstack((proposed_params, proposed_hyperparams))
+
+    data = np.array([[1, 2]] * num_posterior_samples)
+    expected_log_like = -1/2 * (2 * np.log(2 * np.pi) + np.log(7) + 2 / 7)
+    expected_log_like = np.full((num_samples, num_posterior_samples),
+                                expected_log_like)
+
+    model = MVNHierarchModel(proposal)
+    log_like = model(data)
+
+    np.testing.assert_array_equal(log_like, expected_log_like)
